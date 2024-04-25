f93a56bff2a7abb5a81202f56754df52b5a10979
Reinaldo Junior <reifsjunior@gmail.com>
2024-04-25 17:37:45 -0300

    update da aula de operadores

---
+++ tipos-vaiaveis/src/Operadores.java
@@ -0,0 +1,109 @@
+public class Operadores {
+
+    public static void main(String[] args) {
+
+        // Operadores Lógicos
+
+        boolean condicao1 = true;
+        boolean condicao2 = true;
+
+        if(condicao1 && (7 > 4)){
+            System.out.println("As duas condições são verdadeiras");
+        }
+        if (condicao1 || condicao2) {
+            System.out.println("Uma das condições é verdadeira");
+        }
+
+        System.out.println("FIM");
+
+        // Operadores Relacionais
+        /*
+         * String nomeUM = "Reinaldo";
+         * String nomeDois = new String("Reinaldo");
+         * 
+         * System.out.println(nomeUM == nomeDois);
+         * System.out.println(nomeUM.equals(nomeDois));
+         * 
+         * int numero1 = 1;
+         * int numero2 = 2;
+         * 
+         * boolean simNao = numero1 == numero2;
+         * 
+         * if(numero1 < numero2){
+         * System.out.println("A nossa condição é verdadeira");
+         * }
+         * 
+         * System.out.println("numeroUM é igual a numeroDois? " + simNao);
+         * 
+         * simNao = numero1 != numero2;
+         * 
+         * System.out.println("numeroUM é diferente a numeroDois? " + simNao);
+         * 
+         * simNao = numero1 > numero2;
+         * 
+         * System.out.println("numeroUM é maior que numeroDois? " + simNao);
+         */
+
+        /*
+         * Operador Ternário
+         * int a, b;
+         * 
+         * a = 5;
+         * b = 5;
+         * 
+         * String resultado = (a==b) ? "verdadeiro" : "false";
+         * 
+         * /*String resultado = "";
+         * if(a==b)
+         * resultado = "verdadeiro";
+         * else
+         * resultado = "falso";
+         * 
+         * 
+         * System.out.println(resultado);
+         */
+
+        // boolean variavel = true;
+
+        // variavel = !variavel;
+
+        // System.out.println(variavel);
+
+        // int numero = 5;
+
+        // x repetições
+        // numero = numero +1;
+
+        // System.out.println(++numero);
+        // System.out.println(numero);
+
+        // String nomeCompleto = "LINGUAGEM" + " JAVA";
+        // System.out.println(nomeCompleto);
+        /*
+         * String concatenacao ="?";
+         * 
+         * concatenacao = 1+1+1+"1";
+         * System.out.println(concatenacao);
+         * 
+         * concatenacao = 1+"1"+1+1;
+         * System.out.println(concatenacao);
+         * 
+         * concatenacao = 1+"1"+1+"1";
+         * System.out.println(concatenacao);
+         * 
+         * concatenacao = "1"+1+1+1;
+         * System.out.println(concatenacao);
+         * 
+         * concatenacao = "1"+(1+1+1);
+         * System.out.println(concatenacao);
+         * 
+         * int numero = 5;
+         * 
+         * numero = - numero;
+         * System.out.println(numero);
+         * 
+         * numero = numero * -1;
+         * System.out.println(numero);
+         */
+    }
+}
